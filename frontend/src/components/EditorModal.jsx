843beea96508bdde107f2e7786603b6f3f90fe17
vandanabhakuni <vandanabhakuni06@gmail.com>
2025-09-01 22:44:50 +0530

    first commit

---
+++ frontend/src/components/EditorModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditorModal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EditorModal
---
+++ frontend/src/components/Postcard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Postcard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Postcard
